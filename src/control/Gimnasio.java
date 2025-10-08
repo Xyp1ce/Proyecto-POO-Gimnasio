ec1d485322b913480080357e5e46e0d2d0f7c854
Cybercomander <carlosarielureta@gmail.com>
2025-10-08 11:40:14 -0700

    Estructura de carpetas y compiladores

---
+++ src/control/Gimnasio.java
@@ -0,0 +1,11 @@
+package control;
+
+import javax.swing.JOptionPane;
+
+public class Gimnasio{
+    public static void main(String[] args){
+        //Impresion de hola mundo
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+    }
+
+}
