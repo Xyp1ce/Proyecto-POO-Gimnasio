bf9eaeaedec2fc3aaf22be7cf6c0ae78a684eb57
Cybercomander <carlosarielureta@gmail.com>
2025-11-10 11:59:39 -0800

    Clases y persistencia
    Se agregaron las clases del proyecto (tal vez falten para herencia)
    Se inició con la persistencia, falta que funcione.

---
+++ src/control/Persistencia.java
@@ -0,0 +1,60 @@
+package control;
+
+import entidades.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+@SuppressWarnings("unused")
+public class Persistencia {
+	//ATRIBUTOS
+	
+	
+	//GETTERS Y SETTERS
+	
+	
+	
+	//CONSTRUCTORES
+	
+	
+
+	//METODOS
+	//METODO PARA LEER LOS OBJETOS SERIALIZADOS
+	public static Gimnasio getDatos() {	
+		FileInputStream fisPer = null;
+		
+		try {
+			fisPer = new FileInputStream("Gimnasio.ser");
+			ObjectInputStream oisPer = new ObjectInputStream(fisPer);
+			
+			//LEER EL OBJETO SERIALIZADO
+			Gimnasio g = (Gimnasio) oisPer.readObject();
+			return g;
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void setDatos() {
+		FileOutputStream archivo;
+		File f =  new File("Gimnasio.ser");
+		
+		try {
+			archivo = new FileOutputStream(f, false);
+			ObjectOutputStream oos = new ObjectOutputStream(archivo);
+			oos.writeObject(Gimnasio.class);
+			oos.close();
+			archivo.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+}
